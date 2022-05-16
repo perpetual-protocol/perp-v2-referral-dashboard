692e83f9268bd53a55bfd8df1942fa4f75f311f2
justin <>
2022-05-16 21:38:37 +0800

    add torus and coinbase wallet

--- src/components/WalletConnect.tsx
+++ src/components/WalletConnect.tsx
@@ -1,5 +1,7 @@
 import { useWeb3React } from "@web3-react/core";
 import { InjectedConnector } from "@web3-react/injected-connector";
+import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+import { TorusConnector } from "@web3-react/torus-connector";
 import React from "react";
 import { useState } from "react";
 import Jazzicon from "react-jazzicon";
@@ -10,6 +12,9 @@ import WalletConnectLogo from "../assets/walletconnect.svg";
 import Wallet from "../assets/wallet.svg";
 
 import MetamaskLogo from "../assets/metamask.svg";
+import TorusLogo from "../assets/torus.svg";
+import CoinbaseLogo from "../assets/coinbase.svg";
+
 import { useEffect } from "react";
 import { useGlobalState } from "../AppStateHolder";
 import { WalletConnectConnector } from "../utils/WalletConnectConnector";
@@ -24,26 +29,73 @@ const RPC_URLS = {
 
 export enum ConnectorNames {
   Injected = "Metamask",
-  WalletConnect = "WalletConnect",
+  WalletConnect = "Wallet Connect",
+  WalletlinkConnect = "Coinbase Wallet",
+  TorusConnect = "Torus Wallet",
 }
 
 const LogoMap = {
   [ConnectorNames.Injected]: MetamaskLogo,
   [ConnectorNames.WalletConnect]: WalletConnectLogo,
+  [ConnectorNames.WalletlinkConnect]: CoinbaseLogo,
+  [ConnectorNames.TorusConnect]: TorusLogo,
 };
 
-export const injected = new InjectedConnector({
-});
+export const injected = new InjectedConnector({});
 
 export const walletconnect = new WalletConnectConnector({
   supportedChainIds: [10],
   qrcode: true,
   rpc: RPC_URLS,
 });
+export const walletLinkConnect = new InjectedConnector({});
+new WalletLinkConnector({
+  url: RPC_URLS[10],
+  appName: "perp-exchange",
+});
 
+export const torusConnect = new TorusConnector({
+  chainId: 10,
+  initOptions: {
+    network: {
+      host: RPC_URLS[10],
+      chainId: 10,
+    },
+  },
+});
+export const activateInjectedProvider = (
+  providerName = ConnectorNames.Injected
+) => {
+  const { ethereum } = window;
+
+  if (!ethereum?.providers) {
+    return undefined;
+  }
+  console.log("providerName", providerName);
+
+  let provider;
+  switch (providerName) {
+    case ConnectorNames.WalletlinkConnect:
+      provider = ethereum.providers.find(
+        ({ isCoinbaseWallet }: any) => isCoinbaseWallet
+      );
+      break;
+    case ConnectorNames.Injected:
+      provider = ethereum.providers.find(({ isMetaMask }: any) => isMetaMask);
+      break;
+    default:
+      provider = ethereum.providers.find(({ isMetaMask }: any) => isMetaMask);
+  }
+
+  if (provider) {
+    ethereum.setSelectedProvider(provider);
+  }
+};
 export const connectorsByName: { [connectorName in ConnectorNames]: any } = {
   [ConnectorNames.Injected]: injected,
   [ConnectorNames.WalletConnect]: walletconnect,
+  [ConnectorNames.WalletlinkConnect]: walletLinkConnect,
+  [ConnectorNames.TorusConnect]: torusConnect,
 };
 
 function shortenAddress(str) {
@@ -54,32 +106,68 @@ export default function WalletConnect({ size = "sm" }: Props) {
   const { activate, deactivate, error, active } = useWeb3React();
   const [isModalVisible, setIsModalVisible] = useState(false);
   const { canAccessApp, account, setViewOnlyAddress } = useGlobalState();
+  useEffect(() => {
+    console.log("=====");
+    // async function test() {
+    //   await import("@coinbase/wallet-sdk")
+    //     .then((m) => {
+    //       console.log("dyninic", m);
 
+    //       return m?.default ?? m;
+    //     })
+    //     .then((coinbaseSD) => {
+    //       coinbaseSD;
+    //     })
+    //     .catch((error) => console.error("error", error));
+    // }
+
+    // test();
+    // async function test() {
+    //   await walletLinkConnect.activate().then(console.log).catch("test", error);
+    // }
+    // test();
+  }, []);
   useEffect(() => {
     console.error("err", error);
   }, [error]);
 
-  const activateWallet = (name: ConnectorNames) => {
+  const activateWallet = async (name: ConnectorNames) => {
+    console.log("name", name);
+
+    if (
+      name === ConnectorNames.Injected ||
+      name === ConnectorNames.WalletlinkConnect
+    ) {
+      activateInjectedProvider(name);
+    }
     setIsModalVisible(false);
-    activate(connectorsByName[name]);
+    await activate(
+      connectorsByName[name],
+      (err) => {
+        console.error("activate" + err);
+      },
+      true
+    );
   };
 
   const disconnect = () => {
     if (active) {
       deactivate();
     }
-    setViewOnlyAddress('');
+    setViewOnlyAddress("");
     setIsModalVisible(false);
   };
 
   const closeModal = () => {
     setIsModalVisible(false);
-  }
+  };
 
   const buttonType = canAccessApp ? "secondary" : "primary";
   const icon = canAccessApp ? (
     <Jazzicon diameter={17} seed={parseInt(account)} />
-  ) : <Wallet />;
+  ) : (
+    <Wallet />
+  );
 
   return (
     <div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./App";
-import './styles/tailwind.css'; 
+import "./styles/tailwind.css";
 
-render(<App/>, document.getElementById("root"));
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+render(<App />, document.getElementById("root"));
