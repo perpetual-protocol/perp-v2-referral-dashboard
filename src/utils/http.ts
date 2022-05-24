d0172bf33f6fee0123dec2ae229cc63e9c5d45fe
justin <>
2022-05-24 16:43:20 +0800

    feat: change subgraph to self-hosted

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,22 +1,19 @@
-import axios from 'axios';
+import axios from "axios"
 
 export const POST = async (url: string, body: Record<string, any>) => {
-  return await (
-    await axios.post(url, body)
-  ).data;
-};
+    return await (
+        await axios.post(url, body)
+    ).data
+}
 
 export const SUBGRAPH = (query: string) => {
-  return POST('https://api.thegraph.com/subgraphs/name/perpetual-protocol/perpetual-v2-optimism', {
-    query
-  });
-};
+    return POST("https://subgraph-api-singapore.perp.fi/subgraphs/name/perpetual-protocol/perpetual-v2-optimism", {
+        query,
+    })
+}
 
 export const STAKED_SUBGRAPH = (query: string) => {
-  return POST(
-    'https://api.thegraph.com/subgraphs/name/1saf/perpetual-staking-subgraph',
-    {
-      query
-    }
-  );
-};
+    return POST("https://api.thegraph.com/subgraphs/name/1saf/perpetual-staking-subgraph", {
+        query,
+    })
+}
