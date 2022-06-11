72d584e1ddafc13606f90c3c1609f67e241b230e
justin <>
2022-06-11 14:45:23 +0800

    feat: add more blacklist

---
+++ src/views/report/constants.ts
@@ -0,0 +1,5 @@
+export const rewardsFields = ["referee", "usd_cap", "totalFeesPaid", "rebateUSD"]
+
+export const referrerRewardsFields = ["referrer", "code", "usd_cap", "rebateUSD"]
+export const blacklistedCodes = ["perpofficial", "perpmaker", "perparb"]
+export const PAGE_SIZE = 1000
